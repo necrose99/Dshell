5a0fe4ea97751edd3a2a925ab676618b3ddb31c0
Michael R. Lawrence <mike@michaellawrenceit.com>
2015-11-18 20:31:36 -0600

    Update install-gentoo.py

--- install-gentoo.py
+++ install-gentoo.py
@@ -22,12 +22,17 @@ for module, pkg in dependencies.items():
         print("{} is installed".format(module))
 
 if missing_pkgs:
-    cmd = ["emerge --sync --quiet && emerge -v",] + missing_pkgs  #  Emerge -av --ask --verbose else emerge foo --quiet to shut up build/emerge messages
-# emerge --sync --quiet and or emerge-webrsync --quiet gets all the updates for ebuild scripts/security/metadata/etc 
-    print(" ".join(cmd))
-    call(cmd)
-
-call(["make", "all"]) ## Gentoo way is offer a choice make rc,init 
+    print("Hello, Calling Emerge to Build packages that are missing")
+    cmd = ["emerge --sync --quiet && emerge -v",] + missing_pkgs 
+    call(["make", "all"]) 
+#Notes to DSHELL TEAM. can TRIM this DOWN if you wish. this is hear as a conviniance to you. 
+#  Emerge -av --ask --verbose else emerge foo --quiet to shut up build/emerge messages
+# emerge --sync --quiet and or emerge-webrsync --quiet gets all the updates for ebuild scripts/security/metadata/etc , updates to portage spam the console, 
+# --quiet or remove for the reasuring flurry of text , emerge -v (verbose) , emerge -av give promt to user.
+# USE="+DOC +KDE -GTK -VLC" emerge foo-basket? some?package to enable + , DISABLE use-flag -
+## Gentoo way is offer a choice make rc,init 
 # then offer docs via USE="+docks in ebuilds when calling python or disutils.eclass's 
 # I pruned the Do you want the docs asker below too much HELL. 
-
+# for Sabayon (Gentoo overlay , Binary Distro of Gentoo) USers will need to Run equo i + missing_pkgs & or allow the emerge to post
+print("Emerge compleated, NOTE if you are a Sabayon User (Gentoo-overlay/BIN-overlay) you will Likely whish to resync Portage to Entropy")
+print("equo rescue spmsync, to do this , else entropy; Your binary pkg-manager may not be aware that is is installed")
